b8a888882cc952cdf2841331419999ca9266c72d
KalviSommer <kalvi005@gmail.com>
2021-12-10 14:54:08 +0200

    commit

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -1,23 +1,74 @@
 <template>
-  <div class="SecondPage" style="text-align: left; margin-left: 100px">
-    <span>User ID: {{userId}}</span><br>
-    <br>
-    <br>
-    <div style="text-align: left; margin-left: 100px">
-     <button v-on:click="setAlert">Create new alert</button><br>
+  <div class="SecondPage">
+
+    <div style="text-align: left; margin-left: 100px; float: left">
+      <span>User ID: {{ userId }}</span><br>
+      <br>
+      <br>
+      <button v-on:click="setAlert">Create new alert</button>
+      <br>
+      <br>
+      <button>Choose symbol</button><br>
+      1=BTC 2=ETC
+      <select v-model="symbol">
+        BTC<option>1</option>
+        ETC<option>2</option>
+      </select>
+      <br>
+      <br>
+      <button>RSI timeframe</button>
+      <select v-model="timeframe">
+        <option>1D</option>
+      </select>
+      <br>
+      <br>
+      <button>RSI sell filter</button>
+      <select v-model="rsifilter">
+        <option>40</option>
+        <option>35</option>
+        <option>30</option>
+        <option>25</option>
+        <option>20</option>
+      </select><br>
+      <br>
+      <br>
+      <button v-on:click="alertParams">Add to table</button>
+      <br>
       <br>
     </div>
-    <button>Choose symbol</button><br>
-      <br>
-
-      <button>RSI timeframe</button><br>
-      <br>
-      <button>RSI filter</button><br>
-      <br>
-      <button>Add to table</button><br>
-      <br>
-
-    <button class="Center">Demo</button>
+    <div style="text-align: right; margin-right: 100px">
+      <table>
+        <tr>
+          <th></th>
+          <th>Symbol</th>
+          <th>Current price</th>
+          <th>Current RSI</th>
+          <th>RSI timeframe</th>
+          <th>RSI filter</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>rrt</td>
+          <td>rtr</td>
+          <td>trtr</td>
+        </tr>
+        <tr>
+          <td>rrt</td>
+          <td>rtr</td>
+          <td>trtr</td>
+        </tr>
+        <tr>
+          <td>rrt</td>
+          <td>rtr</td>
+          <td>trtr</td>
+        </tr>
+        <tr>
+          <td>rrt</td>
+          <td>rtr</td>
+          <td>trtr</td>
+        </tr>
+      </table>
+    </div>
 
 
   </div>
@@ -29,24 +80,26 @@ import router from "@/router";
 export default {
   data: function () {
     return {
-      answer: "",
       userId: "",
-      userIdAns: {}
+      postedParams: "",
+      symbol: "",
+      timeframe: "",
+      rsifilter: "",
+
     }
   },
 
   methods: {
-    setAlert: function (){
-
-    },
-    getSelectedUser: function () {
-      this.$http.get('rsiadvisor/getuser/' + this.userId)
+    alertParams: function () {
+      this.$http.post('rsiadvisor/alertParams/' + this.symbol + "/" + this.userId+ "/"
+          + this.rsifilter + "/" + this.timeframe)
           .then(response => {
-            this.userIdAns = response.data
+            console.log(this.alertParams)
+            this.postedParams = response.data
           })
     },
-    goToRegister: function () {
-      router.push({name: '3page'})
+    setAlert: function () {
+
     },
     goToDashboard: function () {
       router.push({name: 'SecondPage'})
@@ -60,8 +113,10 @@ export default {
 </script>
 
 <style scoped>
-input {background: #a29b97
+input {
+  background: #a29b97
 }
+
 table {
   text-decoration-color: #26e808;
   background-color: burlywood;
@@ -69,12 +124,19 @@ table {
   margin-right: auto;
 
 }
-td{color: #282626;
+
+td {
+  color: #282626;
   background-color: darkkhaki;
 }
-button {color-adjust: economy;
+
+button {
+  color-adjust: economy;
   color: #030303;
   background-color: #d7690f;
-  }
-img {margin-left: unset}
+}
+
+img {
+  margin-left: unset
+}
 </style>
